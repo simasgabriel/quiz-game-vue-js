0f6dc41359a33bdcc61e76f70194c4334f8d7b36
simasgabriel <45041309+simasgabriel@users.noreply.github.com>
2023-05-16 16:25:32 -0300

    inital structure of app; added bootstrap

---
+++ quiz game vue/src/App.vue
@@ -0,0 +1,44 @@
+
+
+
+
+
+<template>
+  
+  <div>
+    <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima reiciendis quasi unde iste perferendis quam? Minima esse totam similique dolorum saepe delectus, facere nobis numquam laudantium iure voluptas a aliquid?</h1>
+  
+
+    <div class="form-check">
+  <input class="form-check-input" type="radio" name="options"  value="True" >
+  <label class="form-check-label" >
+    True
+  </label>
+</div>
+
+  
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="options"  value="False" >
+  <label class="form-check-label" >
+    False
+  </label>
+</div>
+
+
+
+
+  <button class="send btn btn-success" type="button"> Send</button><br>
+  </div>
+ 
+</template>
+
+<style >
+#app{
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: justify;
+  padding: 10px;
+  margin: 10em ;
+  margin-bottom: 1rem;
+
+}
+</style>
